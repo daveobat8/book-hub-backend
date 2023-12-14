8f24fe65992358fd8396586fc7822431cf49456b
David <dave.obat@student.moringashool.com>
2023-12-14 21:17:13 +0300

    set up

--- main.py
+++ main.py
@@ -7,3 +7,27 @@ app= FastAPI()
 @app.get('/')
 def index():
     return {"message": "Welcome to my first api"}
+#get all books
+@app.get('/books')
+def books():
+    return []
+
+#return a single book
+@app.get('/books/{book_id}')
+def books():
+    return {}
+
+#create/add a book
+@app.post('/books')
+def create():
+    return {"message":"Book added succesfully"}
+
+#update a book
+@app.patch('/books/{book_id}')
+def update_book(book_id: int):
+    return {"message": f"Book {book_id} added successfully"}
+
+#delete a book
+@app.delete('/books/{book_id}')
+def delete_book(book_id: int):
+    return {"message": f"Book {book_id} deleted succesfully"}
