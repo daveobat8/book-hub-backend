a783fc1a6d72062908941e1106fcc13918f27215
David <dave.obat@student.moringashool.com>
2023-12-14 22:05:44 +0300

    set up db connection

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# connect to out postgre database
+engine = create_engine("postgres://admin:zPnxxBE0m5LNcZpFefEnjSKY2OUMyEW4@dpg-cltkuoq1hbls738lgt50-a.frankfurt-postgres.render.com/book_vwik")
+
+#create a connection with sessionmaker
+SessionLocal = sessionmaker(bind=engine)
+
+# method to get database
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
